6de4e0858f7a1c7253c1778adf4b313302a7e57f
danishsiddiqui994 <danish994@outlook.com>
2023-09-25 18:38:55 +0500

    forEach and map practice

---
+++ app.js
@@ -0,0 +1,54 @@
+
+// ye for Each ka function hai
+
+
+// const People = [
+// {name:"akhtar", age:37 ,position:"boss"},
+// {name:"abid", age:41, position:"manager"},
+// {name:"kashif", age:45 ,position:"supervisor"},
+
+//  ];
+
+//  People.forEach(function(item){
+// console.log(item.position)
+
+
+
+//  });
+
+//  forEach kehta hai mey return nhi karo ga na array change kro ga
+// map kehta hai return do ga aur original array ka size change nhi kro ga 
+
+// ------------------------------------------------------------------------------
+
+// const People = [
+//     {name:"akhtar", age:37 ,position:"boss"},
+//     {name:"abid", age:41, position:"manager"},
+//     {name:"kashif", age:45 ,position:"supervisor"},
+    
+//      ];
+
+//      const newValue=People.map(function(person){
+
+//         console.log(person)
+
+
+
+
+//      });
+    // ------------------------------------------------------------------------
+
+//     const People = [
+//             {name:"akhtar", age:37 ,position:"boss"},
+//             {name:"abid", age:41, position:"manager"},
+//             {name:"kashif", age:45 ,position:"supervisor"},
+            
+//              ];
+
+
+// const ages= People.map(function(person){
+// return person.age + 20;
+
+
+// });
+// console.log(ages);
